da66ce7cd12a5bd9d3305ad2298cf35d90207e37
Kauko <kauko@biosynth.link>
2020-06-10 22:26:20 -0700

    Fix Twitch embed

--- app/sites.js
+++ app/sites.js
@@ -4,13 +4,15 @@ const site_picarto = require("./site_helper/picarto");
 
 // Constants
 
+const current_domain = chrome.runtime.id;
+
 exports.models = [
-    {"name": "YouTube", "embed": "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/%s\" frameborder=\"0\" allowfullscreen></iframe>"},
-    {"name": "Twitch", "embed": "<iframe src=\"https://player.twitch.tv/?channel=%s&muted=true\" frameborder=\"0\" scrolling=\"no\" height=\"100%\" width=\"100%\"></iframe>", "hasChat": true},
-    {"name": "Twitch Chat", "embed": "<iframe src=\"https://www.twitch.tv/%s/chat?popout=\" frameborder=\"0\" scrolling=\"no\" height=\"100%\" width=\"100%\"></iframe>"},
-    {"name": "Picarto", "embed": "<iframe src=\"https://picarto.tv/streampopout/%s/public\" frameborder=\"0\" scrolling=\"no\" height=\"100%\" width=\"100%\"></iframe>", "hasChat": true},
-    {"name": "Picarto Chat", "embed": "<iframe src=\"https://picarto.tv/chatpopout/%s/public\" frameborder=\"0\" scrolling=\"no\" height=\"100%\" width=\"100%\"></iframe>"},
-    {"name": "Tigerdile", "embed": "<iframe src=\"https://www.tigerdile.com/stream/%s?single=1&popout=1\" frameborder=\"0\" scrolling=\"no\" height=\"100%\" width=\"100%\"></iframe>"},
+    {"name": "YouTube", "embed": `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/%s" frameborder="0" allowfullscreen></iframe>`},
+    {"name": "Twitch", "embed": `<iframe src="https://player.twitch.tv/?channel=%s&muted=true&parent=${current_domain}" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>`, "hasChat": true},
+    {"name": "Twitch Chat", "embed": `<iframe src="https://www.twitch.tv/embed/%s/chat?parent=${current_domain}" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>`},
+    {"name": "Picarto", "embed": `<iframe src="https://picarto.tv/streampopout/%s/public" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>`, "hasChat": true},
+    {"name": "Picarto Chat", "embed": `<iframe src="https://picarto.tv/chatpopout/%s/public" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>`},
+    {"name": "Tigerdile", "embed": `<iframe src="https://www.tigerdile.com/stream/%s?single=1&popout=1" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>`},
     {"name": "Custom", "embed": "%s"},
 ];
 
